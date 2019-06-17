0df6318093d0b883ec9d9eaa2a182fb7b4aa42ee
LeoRoma <xiajtn@gmail.com>
2019-06-17 21:50:24 +0100

    exercise01

--- directory.rb
+++ directory.rb
@@ -23,8 +23,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |index, student|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
