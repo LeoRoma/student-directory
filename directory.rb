07d518e565da049bc7a29784e983dd52bab4bbe7
LeoRoma <xiajtn@gmail.com>
2019-06-17 22:15:53 +0100

    step8

--- directory.rb
+++ directory.rb
@@ -1,19 +1,19 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+  
   students = []
-  # get the first name
+
   name = gets.chomp
-  # while the name is not empty, repeat this code
+
   while !name.empty? do
-    # add the student hash to the array
+
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    # get another name from the user
+
     name = gets.chomp
   end
-  # return the array of students
+
   students
 end
 
@@ -24,7 +24,7 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
   end
 end
 
@@ -33,7 +33,7 @@ def print_footer(students)
 end
 
 students = input_students
-#nothing happens until we call the methods
+
 print_header
 print(students)
 print_footer(students)
