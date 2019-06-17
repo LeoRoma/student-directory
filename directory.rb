7f25dbad0f46737dc2d665f1e58766ba2bca043e
LeoRoma <xiajtn@gmail.com>
2019-06-17 22:23:00 +0100

    exercise07

--- directory.rb
+++ directory.rb
@@ -1,19 +1,20 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+
   students = []
-  # get the first name
+
   name = gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
+    cohort = gets.chomp
+    if cohort == nil || cohort == ""
+    cohort = "november"
+    end
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
-    # get another name from the user
+
     name = gets.chomp
   end
-  # return the array of students
   students
 end
 
