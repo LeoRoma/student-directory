56d807d7b4708fd73944e7897ef21c3cc1559e33
LeoRoma <xiajtn@gmail.com>
2019-06-18 09:22:08 +0100

    saving the data

--- directory.rb
+++ directory.rb
@@ -9,6 +9,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. save the list to students.csv"
   puts "9. Exit"
 end
 def show_students
@@ -22,6 +23,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -52,7 +55,17 @@ def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    files.puts csv_line
+  end
+  file.close
+end
 def print_student_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
