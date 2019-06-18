90ba30cb300f3b303a82871a6702de41f73d91b1
LeoRoma <xiajtn@gmail.com>
2019-06-18 10:27:19 +0100

    loading the data

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. save the list to students.csv"
+  puts "4. load the list from students.csv"
   puts "9. Exit"
 end
 def show_students
@@ -25,6 +26,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -56,13 +59,22 @@ def print_header
   puts "-------------"
 end
 def save_students
-  #open the file for writing
+  # open the file for writing
   file = File.open("students.csv", "w")
-  #iterate over the array of students
+  # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
-    files.puts csv_line
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
